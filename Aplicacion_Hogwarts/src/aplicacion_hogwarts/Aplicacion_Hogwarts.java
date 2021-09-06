1107fcfc7b7d06cec62a446722dc36da5989cb75
Ingrid E <ingrid.echeverri@correounivalle.edu.co>
2021-09-06 10:48:27 -0500

    Iniciando el proyecto

---
+++ Aplicacion_Hogwarts/src/aplicacion_hogwarts/Aplicacion_Hogwarts.java
@@ -0,0 +1,28 @@
+/*
+ * @author Ingrid Echeverri (https://github.com/Ingrid-E)
+ * @version
+ * @file aplicacion_hogwarts
+ * @date    
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package aplicacion_hogwarts;
+
+/**
+ * Main class
+ * @returns
+ */
+public class Aplicacion_Hogwarts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
